3c4288c9198a760a216852407cb1131cd2c3dcc8
shahira-sadat <shahira.sadat1@gmail.com>
2022-09-13 09:39:13 +0430

    Add queries

---
+++ queries.sql
@@ -0,0 +1,17 @@
+/*Queries to answers the questions from projects requirements*/
+
+SELECT name FROM animals WHERE name LIKE '%mon';
+
+SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND  '2019-12-31';
+
+SELECT name FROM animals WHERE neutered = TRUE AND escape_attempts < 3;
+
+SELECT date_of_birth FROM animals WHERE name='Agumon' OR  name='Pikachu';
+
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+
+SELECT * FROM animals WHERE neutered = true;
+
+SELECT * FROM animals WHERE name NOT LIKE 'Gabumon';
+
+SELECT * FROM animals WHERE weight_kg  >= 10.4 AND weight_kg <= 17.3;
