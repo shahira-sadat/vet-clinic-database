ed1af5ce61437c1f2b9aa5b7ecd03f04bd9720f1
shahira-sadat <shahira.sadat1@gmail.com>
2022-09-13 22:31:56 +0430

    Add queries

--- queries.sql
+++ queries.sql
@@ -15,3 +15,78 @@ SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name NOT LIKE 'Gabumon';
 
 SELECT * FROM animals WHERE weight_kg  >= 10.4 AND weight_kg <= 17.3;
+
+/* Part 1 */
+
+BEGIN;
+
+UPDATE animals
+SET species = 'unspecified';
+
+ROLLBACK;
+
+/* Part 2 */
+
+BEGIN;
+
+UPDATE animals
+SET species = 'digimon' WHERE name LIKE '%mon';
+
+UPDATE animals
+SET species = 'pokemon' WHERE species IS NULL;
+
+COMMIT;
+
+/* Part 3 */
+
+BEGIN;
+
+DELETE FROM animals;
+
+ROLLBACK;
+
+/* Part 4 */
+
+BEGIN;
+
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+
+SAVEPOINT sp1;
+
+UPDATE animals
+SET weight_kg = weight_kg * -1;
+
+ROLLBACK TO SAVEPOINT sp1;
+
+UPDATE animals
+SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+
+COMMIT;
+
+/* Part 5 */
+
+/* How many animals are there? */
+SELECT COUNT(*) FROM animals;
+
+/* How many animals have never tried to escape? */
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+
+/* What is the average weight of animals? */
+SELECT AVG(weight_kg) FROM animals;
+
+/* Who escapes the most, neutered or not neutered animals? */
+SELECT CASE sub.neutered
+WHEN sub.neutered = TRUE THEN 'Neutered escaped the most'
+ELSE 'Not Neutered escaped the most'
+END
+FROM (
+  SELECT neutered, SUM(escape_attempts) AS res FROM animals GROUP BY neutered
+) AS sub
+ORDER BY res DESC
+LIMIT 1;
+
+/* What is the minimum and maximum weight of each type of animal? */
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+
+/* What is the average number of escape attempts per animal type of those born between 1990 and 2000? */
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth >= '1990-01-01' AND date_of_birth < '2001-01-01' GROUP BY species;
