c10b3dfb07dc5e21d9f4e4f14b22abaea562a490
Shahira Sadat <53530780+shahira-sadat@users.noreply.github.com>
2022-09-19 14:32:20 +0430

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,28 +1,30 @@
-/* Insert sample data in to database */
+/* Populate database with sample data. */
+/*Vet clinic database: create animals table*/
+INSERT INTO animals (id,name, date_of_birth , weight_kg, neutered, escape_attempts )
+VALUES (1,'Agumon', '2020-02-03',10.23,TRUE,0);
 
-INSERT INTO animals (id, name, date_of_birth , escape_attempts, neutered, weight_kg) 
-VALUES (1,'Agumon', '2020-02-03', 0, TRUE, 10.23);
+INSERT INTO animals (id,name, date_of_birth , weight_kg, neutered, escape_attempts )
+VALUES (2,'Gabumon', '2018-11-15',8,TRUE, 2);
 
-INSERT INTO animals (id, name, date_of_birth , escape_attempts, neutered, weight_kg) 
-VALUES (2,'Gabumon', '2018-11-15', 2, TRUE, 8);
+INSERT INTO animals (id,name, date_of_birth , weight_kg, neutered, escape_attempts )
+VALUES (3,'Pikachu', '2021-01-07',15.04,FALSE,1);
 
-INSERT INTO animals (id, name, date_of_birth , escape_attempts, neutered, weight_kg) 
-VALUES (3,'Pikachu', '2021-01-07', 1, FALSE, 15.04);
+INSERT INTO animals (id,name, date_of_birth , weight_kg, neutered, escape_attempts )
+VALUES (4,'Devimon', '2017-05-12',11,TRUE,5);
 
-INSERT INTO animals (id, name, date_of_birth , escape_attempts, neutered, weight_kg) 
-VALUES (4,'Devimon', '2017-05-12', 5, TRUE, 11);
+/* Vet clinic database: query and update animals table */
+INSERT INTO animals (id,name, date_of_birth , weight_kg, neutered, escape_attempts )
+VALUES (5,'Charmander', '2020-02-08',-11,FALSE,0),
+       (6,'Plantmon', '2021-11-15',-5.7,TRUE, 2), 
+       (7,'Squirtle', '1993-04-02',-12.13,FALSE,3),
+       (8,'Angemon', '2005-06-12',-45,TRUE,1),
+       (9,'Boarmon', '2005-06-7',20.4,TRUE,7),
+       (10,'Blossom', '1998-10-13',17,TRUE,3),
+       (11,'Ditto', '2022-05-14', 22,TRUE,4);
 
-
-INSERT INTO animals (id, name, date_of_birth , escape_attempts, neutered, weight_kg) 
-VALUES (5,'Charmander', '2020-02-08', 0, FALSE, -11),
-       (6,'Plantmon', '2021-11-15', 2, TRUE, -5.7), 
-       (7,'Squirtle', '1993-04-02', 3, FALSE, -12.13),
-       (8,'Angemon', '2005-06-12', 1, TRUE, -45),
-       (9,'Boarmon', '2005-06-7', 7, TRUE, 20.4),
-       (10,'Blossom', '1998-10-13', 3, TRUE, 17),
-       (11,'Ditto', '2022-05-14', 4, TRUE, 22);
-
-
+/*Vet clinic database: query multiple tables*/
+/*Task1
+Insert the following data into the owners table*/
 INSERT INTO owners (full_name, age)                                                    
 VALUES('Sam Smith', 34),
       ('Jennifer Orwell', 19),
@@ -31,26 +33,34 @@ VALUES('Sam Smith', 34),
       ('Dean Winchester', 14),
       ('Jodie Whittaker', 38);
 
+/*Task2
+Insert the following data into the species table*/
 INSERT INTO species (name)                                                    
 VALUES('Pokemon'),
       ('Digimon');
 
+/*Task3
+Modify your inserted animals so it includes the species_id value*/
+-- If the name ends in "mon" it will be Digimon
 UPDATE animals SET species_id = 2  WHERE name LIKE '%mon'; 
 
+-- All other animals are Pokemon
 UPDATE animals                                                                         
 SET species_id = 1 WHERE species_id IS NULL;
 
+/*Task4
+Modify your inserted animals to include owner information (owner_id)*/
 UPDATE animals SET owner_id = 1 WHERE name = 'Agumon';
 UPDATE animals SET owner_id = 2 WHERE name IN ('Gabumon', 'Pikachu');
 UPDATE animals SET owner_id = 3 WHERE name IN ('Devimon', 'Plantmon');
 UPDATE animals SET owner_id = 4 WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
 UPDATE animals SET owner_id = 5 WHERE name IN ('Angemon', 'Boarmon');
 
-
-/* add "join table" for visits */
+/* Vet clinic database: add "join table" for visits */
+/*Task 1 insert into vets*/
 
 INSERT INTO vets (name, age, date_of_graduation)                                                    
-VALUES('William Tatcher', 45 '2000-04-23'),
+VALUES('William Tatcher', 45, '2000-04-23'),
       ('Maisy Smith', 26, '2019-01-17'),
       ('Stephanie Mendez', 64, '1981-05-04'),
       ('Jack Harkness', 38, '2008-06-08');
@@ -61,7 +71,6 @@ VALUES
 ((SELECT id FROM vets WHERE name = 'Stephanie Mendez'),(SELECT id FROM species WHERE name = 'Pokemon')),
 ((SELECT id FROM vets WHERE name = 'Stephanie Mendez'),(SELECT id FROM species WHERE name = 'Digimon')),
 ((SELECT id FROM vets WHERE name = 'Jack Harkness'),(SELECT id FROM species WHERE name = 'Digimon'));
-
 
 INSERT INTO visits (animal_id, vet_id, visit_date) 
 VALUES ((SELECT id FROM animals WHERE name = 'Agumon'), (SELECT id FROM vets WHERE name = 'William Tatcher'), 'May 24, 2020'),
@@ -84,3 +93,4 @@ VALUES ((SELECT id FROM animals WHERE name = 'Agumon'), (SELECT id FROM vets WHE
       ((SELECT id FROM animals WHERE name = 'Boarmon'), (SELECT id FROM vets WHERE name = 'Maisy Smith'), 'August 3, 2020'),
       ((SELECT id FROM animals WHERE name = 'Blossom'), (SELECT id FROM vets WHERE name = 'Stephanie Mendez'), 'May 24, 2020'),
       ((SELECT id FROM animals WHERE name = 'Blossom'), (SELECT id FROM vets WHERE name = 'William Tatcher'), 'January 11, 2021');
+      
