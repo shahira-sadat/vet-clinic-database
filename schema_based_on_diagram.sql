5d0ee3e24d7bf6385bff6af95ad6359dc41bea59
shahira-sadat <shahira.sadat1@gmail.com>
2022-09-21 05:50:36 +0430

    Create trearments_medicalhestories(m:n relation)

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -19,7 +19,7 @@ CREATE TABLE medical_histories (
 /* create table invoices */
 CREATE TABLE invoices (
     id BIGSERIAL NOT NULL PRIMARY KEY,
-    medical_history_id BIGINT NOT NULL REFERENCES medical_histories(id) PRIMARY KEY,
+    medical_history_id BIGINT NOT NULL REFERENCES medical_histories(id),
     total_amount DECIMAL(10,2) NOT NULL,
     generated_at TIMESTAMP NOT NULL,
     payed_at TIMESTAMP NOT NULL
@@ -31,3 +31,10 @@ CREATE TABLE treatments (
     type VARCHAR(50) NOT NULL,
     name VARCHAR(100) NOT NULL
 );
+
+/* create table trearments_medicalhestories(m:n relation) */
+CREATE TABLE trearments_medicalhestories (
+    trearment_id INT NOT NULL REFERENCES trearments(id),
+    medical_history_id INT NOT NULL REFERENCES medical_histories(id)
+);
+
